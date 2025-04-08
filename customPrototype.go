e1a858e6a60deaa05d339019cba20604ffca6cab
Nic Fornicola <nicfornicola@gmail.com>
2025-04-08 16:44:34 -0500

    get theme by name, Recommendations is now an array of songIds

--- customPrototype.go
+++ customPrototype.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"reflect"
 
 	"github.com/hyperjumptech/grule-rule-engine/ast"
 	"github.com/hyperjumptech/grule-rule-engine/builder"
@@ -21,58 +22,48 @@ type UserSelections struct {
 	HeartBreak         bool
 	Lessons            bool
 	Rebellion          bool
-	Recommendations    string
+	Recommendations    []string
 }
+
+func (p *UserSelections) GetField(fieldName string) (bool, error) {
+	val := reflect.ValueOf(p).Elem()
+	field := val.FieldByName(fieldName)
+	if !field.IsValid() {
+		return false, fmt.Errorf("field %s does not exist", fieldName)
+	}
+
+	if field.Kind() == reflect.Bool {
+		return field.Bool(), nil
+	}
+
+	return false, fmt.Errorf("field %s is not a boolean", fieldName)
+}
+
+func (p *UserSelections) Append(songId string) () {
+	p.Recommendations = append(p.Recommendations, songId)
+} 
 
 // Custom function to check if adventure and good times are true
 func (p *UserSelections) IsSongThemeMatch(songThemes ...string) bool {
-
-	const userSelection = "User Selection contained "
+	fmt.Println("=========")
+	fmt.Println("UserSelections")
+	fmt.Println(p)
 
 	matchCount := 0
 	for _, theme := range songThemes {
-		fmt.Println(theme)
+		boolValue, err := p.GetField(theme)
 
-		if theme == "Adventure" && p.Adventure {
-			log.Printf(userSelection + "Adventure")
-			matchCount += 1
-		}
-		if theme == "CarsTrucksTractors" && p.CarsTrucksTractors {
-			log.Printf(userSelection + "CarsTrucksTractors")
-			matchCount += 1
-		}
-		if theme == "Goodtimes" && p.Goodtimes {
-			log.Printf(userSelection + "Goodtimes")
-			matchCount += 1
-		}
-		if theme == "Grit" && p.Grit {
-			log.Printf(userSelection + "Grit")
-			matchCount += 1
-		}
-		if theme == "Home" && p.Home {
-			log.Printf(userSelection + "Home")
-			matchCount += 1
-		}
-		if theme == "Love" && p.Love {
-			log.Printf(userSelection + "Love")
-			matchCount += 1
-		}
-		if theme == "HeartBreak" && p.HeartBreak {
-			log.Printf(userSelection + "HeartBreak")
-			matchCount += 1
-		}
-		if theme == "Lessons" && p.Lessons {
-			log.Printf(userSelection + "Lessons")
-			matchCount += 1
-		}
-		if theme == "Rebellion" && p.Rebellion {
-			log.Printf(userSelection + "Rebellion")
-			matchCount += 1
-		}
-	}
+		if err != nil { panic(err) }
 
+		if boolValue {
+			fmt.Printf(theme + " --- Match found!")
+			matchCount += 1
+		} else {
+			fmt.Printf(theme)
+		}
+
+	fmt.Printf("\nMatches: %d", matchCount)
 	if matchCount > 0 {
-		log.Printf("%d", matchCount)
 		return true
 	}
 
@@ -96,7 +87,7 @@ func main() {
 		HeartBreak:         false,
 		Lessons:            false,
 		Rebellion:          false,
-		Recommendations:    "",
+		Recommendations:    []string{},
 	}
 
 	dataCtx.Add("UserSelections", userSelections)
@@ -107,7 +98,7 @@ func main() {
         when
            UserSelections.IsSongThemeMatch("Adventure", "America", "Home", "Lessons")
         then
-            UserSelections.Recommendations = "10000";
+            UserSelections.Append("10000");
             Retract("Check10000");
     }
     `
@@ -129,5 +120,16 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("Song recommendations for user: " + userSelections.Recommendations)
+
+	fmt.Println("=======================================")
+	fmt.Println("Song recommendations for user: ")
+
+	if len(userSelections.Recommendations) > 0 {
+		for _, rec := range userSelections.Recommendations {
+			fmt.Println(rec)
+		}
+
+	} else {
+		fmt.Println("No songs matched the user's selections.")
+	}
 }
